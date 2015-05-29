a1d3f4e2cec6de949b50103009841fdd5e2ece6e
Jonathan Barnes <jbarnes@pivotal.io>
2015-05-29 13:54:42 -0700

    Uses local run-lifecycle task definition instead of shared
    In the effort to not have pipeline reference private tasks we copied the
    shared private task into this repo and have the pipeline reference the
    local version
    
    Addionally:
     - Replaced referencing a private pem file with injecting the private
       key data into the task and creating the pem file from the data passed
       in
     - No longer passes in the cpi_release_name to task since it can be hard
       coded now.
    [#95665146](https://www.pivotaltracker.com/story/show/95665146)
    Signed-off-by: Xing Zhou <xingzhou@cn.ibm.com>

---
+++ ci/tasks/deploy.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+ensure_not_replace_value() {
+  local name=$1
+  local value=$(eval echo '$'$name)
+  if [ "$value" == 'replace-me' ]; then
+    echo "environment variable $name must be set"
+    exit 1
+  fi
+}
+
+ensure_not_replace_value base_os
+ensure_not_replace_value bats_private_key_data
+
+cpi_release_name=bosh-openstack-cpi
+
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
+
+semver=`cat version-semver/number`
+manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
+manifest_filename=$manifest_dir/$base_os-director-manifest.yml
+
+echo "normalizing paths to match values referenced in $manifest_filename"
+mkdir ./tmp
+mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
+mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
+mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
+
+set +x
+echo "$bats_private_key" > ./tmp/bats.pem
+chmod 400 ./tmp/bats.pem
+set -x
+
+initver=$(cat bosh-init/version)
+initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
+chmod +x $initexe
+
+echo "deleting existing BOSH Director VM..."
+$initexe delete $manifest_filename
+
+echo "deploying BOSH..."
+$initexe deploy $manifest_filename
