8c245a43964f8c68d264db795c720bc3bd741747
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2016-02-12 11:39:16 +0100

    Resolve versioned bosh-openstack-cpi release tgz
    [#113161365](https://www.pivotaltracker.com/story/show/113161365)

--- ci/tasks/deploy-e2e-director.sh
+++ ci/tasks/deploy-e2e-director.sh
@@ -41,7 +41,7 @@ director_state_filename="e2e-director-manifest-state.json"
 private_key=${deployment_dir}/e2e.pem
 
 echo "setting up artifacts used in $manifest_filename"
-cp ./bosh-cpi-dev-artifacts/release.tgz ${deployment_dir}/${cpi_release_name}.tgz
+cp ./bosh-cpi-dev-artifacts/${cpi_release_name}-*.tgz ${deployment_dir}/${cpi_release_name}.tgz
 cp ./bosh-release/release.tgz ${deployment_dir}/bosh-release.tgz
 cp ./stemcell/stemcell.tgz ${deployment_dir}/stemcell.tgz
 cp ./director-state-file/${director_state_filename} ${deployment_dir}/${director_state_filename}
