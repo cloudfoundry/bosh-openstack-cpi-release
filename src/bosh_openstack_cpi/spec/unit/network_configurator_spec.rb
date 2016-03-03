6f3309619e997ce1c73cbda403f677642244991f
Beyhan Veli <beyhan.veli@sap.com>
2016-03-03 11:04:09 +0100

    Refactor 'network_configurator_spec.rb'
    - apply rspec style guide for 'describe' and 'context'
    
    [#114099831](https://www.pivotaltracker.com/story/show/114099831)
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- src/bosh_openstack_cpi/spec/unit/network_configurator_spec.rb
+++ src/bosh_openstack_cpi/spec/unit/network_configurator_spec.rb
@@ -1,198 +1,223 @@
 # Copyright (c) 2009-2013 VMware, Inc.
 # Copyright (c) 2012 Piston Cloud Computing, Inc.
 
-require "spec_helper"
+require 'spec_helper'
 
 describe Bosh::OpenStackCloud::NetworkConfigurator do
 
   def set_security_groups(spec, security_groups)
-    spec["cloud_properties"] ||= {}
-    spec["cloud_properties"]["security_groups"] = security_groups
+    spec['cloud_properties'] ||= {}
+    spec['cloud_properties']['security_groups'] = security_groups
   end
 
   def set_nics(spec, net_id)
-    spec["cloud_properties"] ||= {}
+    spec['cloud_properties'] ||= {}
   end
 
   let(:several_manual_networks) do
     spec = {}
-    spec["network_a"] = manual_network_spec
-    spec["network_a"]["ip"] = "10.0.0.1"
-    spec["network_b"] = manual_network_spec
-    spec["network_b"]["cloud_properties"]["net_id"] = "bar"
-    spec["network_b"]["ip"] = "10.0.0.2"
+    spec['network_a'] = manual_network_spec
+    spec['network_a']['ip'] = '10.0.0.1'
+    spec['network_b'] = manual_network_spec
+    spec['network_b']['cloud_properties']['net_id'] = 'bar'
+    spec['network_b']['ip'] = '10.0.0.2'
     spec
   end
 
-  it "should raise an error if the spec isn't a hash" do
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new("foo")
-    }.to raise_error ArgumentError, /Invalid spec, Hash expected,/
-  end
-
-  it "should raise a CloudError if no net_id is extracted for manual networks" do
-    spec = {}
-    spec["network_b"] = manual_network_without_netid_spec
-
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-    }.to raise_error Bosh::Clouds::CloudError, "Manual network must have net_id"
-  end
-
-  it "should raise a CloudError if several manual networks have the same net_id" do
-    spec = several_manual_networks
-    spec["network_b"]["cloud_properties"]["net_id"] = "net"
-
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-    }.to raise_error Bosh::Clouds::CloudError, "Manual network with id net is already defined"
-  end
-
-  it "should raise a CloudError if several dynamic networks are defined" do
-    spec = {}
-    spec["network_a"] = dynamic_network_spec
-    spec["network_b"] = dynamic_network_spec
-
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-    }.to raise_error Bosh::Clouds::CloudError, "Only one dynamic network per instance should be defined"
-  end
-
-  it "should raise a CloudError if several VIP networks are defined" do
-    spec = {}
-    spec["network_a"] = vip_network_spec
-    spec["network_b"] = vip_network_spec
-
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-    }.to raise_error Bosh::Clouds::CloudError, "Only one VIP network per instance should be defined"
-  end
-
-  it "should raise a CloudError if no dynamic or manual networks are defined" do
-    spec = {}
-    spec["network_a"] = vip_network_spec
-
-    expect {
-      Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-    }.to raise_error Bosh::Clouds::CloudError, "At least one dynamic or manual network should be defined"
-  end
-
-  describe "security groups" do
-    it "should be extracted from both dynamic and vip network" do
-      spec = {}
-      spec["network_a"] = dynamic_network_spec
-      set_security_groups(spec["network_a"], %w[foo])
-      spec["network_b"] = vip_network_spec
-      set_security_groups(spec["network_b"], %w[bar])
-
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.security_groups).to eq(%w[bar foo])
+  describe '#initialize' do
+    context 'when spec is not a hash' do
+      it 'should raise an error' do
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new('foo')
+        }.to raise_error ArgumentError, /Invalid spec, Hash expected,/
+      end
     end
 
-    it "should be extracted from both manual and vip network" do
-      spec = {}
-      spec["network_a"] = manual_network_spec
-      set_security_groups(spec["network_a"], %w[foo])
-      spec["network_b"] = vip_network_spec
-      set_security_groups(spec["network_b"], %w[bar])
+    context 'when no net_id defined in manual networks' do
+      it 'should raise a CloudError' do
+        spec = {}
+        spec['network_b'] = manual_network_without_netid_spec
 
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.security_groups).to eq(%w[bar foo])
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error Bosh::Clouds::CloudError, 'Manual network must have net_id'
+      end
     end
 
-    it "should return the default groups if none are extracted" do
-      spec = {}
-      spec["network_a"] = {"type" => "dynamic"}
+    context 'when several manual networks have the same net_id' do
+      it 'should raise a CloudError' do
+        spec = several_manual_networks
+        spec['network_b']['cloud_properties']['net_id'] = 'net'
 
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.security_groups(%w[foo])).to eq(%w[foo])
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error Bosh::Clouds::CloudError, 'Manual network with id net is already defined'
+      end
     end
 
-    it "should return an empty list if no default group is set" do
-      spec = {}
-      spec["network_a"] = {"type" => "dynamic"}
+    context 'when several dynamic networks are defined' do
+      it 'should raise a CloudError' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        spec['network_b'] = dynamic_network_spec
 
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.security_groups).to eq([])
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error Bosh::Clouds::CloudError, 'Only one dynamic network per instance should be defined'
+      end
     end
 
-    it "should raise an error when it isn't an array" do
-      spec = {}
-      spec["network_a"] = dynamic_network_spec
-      set_security_groups(spec["network_a"], "foo")
+    context 'when several VIP networks are defined' do
+      it 'should raise a CloudError' do
+        spec = {}
+        spec['network_a'] = vip_network_spec
+        spec['network_b'] = vip_network_spec
 
-      expect {
-        Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      }.to raise_error ArgumentError, "security groups must be an Array"
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error Bosh::Clouds::CloudError, 'Only one VIP network per instance should be defined'
+      end
+    end
+
+    context 'when only VIP network is defined' do
+      it 'should raise a CloudError' do
+        spec = {}
+        spec['network_a'] = vip_network_spec
+
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error Bosh::Clouds::CloudError, 'At least one dynamic or manual network should be defined'
+      end
     end
   end
 
-  describe "private_ips" do
-    it "should extract private ip address for manual network" do
-      spec = {}
-      spec["network_a"] = manual_network_spec
-      spec["network_a"]["ip"] = "10.0.0.1"
+  describe '#security_groups' do
+    context 'when security_groups are defined in all networks' do
+      it 'should be extracted from all networks' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        set_security_groups(spec['network_a'], %w[foo])
+        spec['network_b'] = vip_network_spec
+        set_security_groups(spec['network_b'], %w[bar])
+        spec['network_c'] = manual_network_spec
+        set_security_groups(spec['network_c'], %w[bla])
 
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.private_ips).to eq(%w[10.0.0.1])
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.security_groups).to eq(%w[bar bla foo] )
+      end
     end
 
-    it "should extract private ip address from manual network when there's also vip network" do
-      spec = {}
-      spec["network_a"] = vip_network_spec
-      spec["network_a"]["ip"] = "10.0.0.1"
-      spec["network_b"] = manual_network_spec
-      spec["network_b"]["ip"] = "10.0.0.2"
+    context 'when no security_groups are defined' do
+      it 'should return the default groups' do
+        spec = {}
+        spec['network_a'] = {'type' => 'dynamic'}
 
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.private_ips).to eq(%w[10.0.0.2])
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.security_groups(%w[foo])).to eq(%w[foo])
+      end
+
+      context 'when no default security_group is set' do
+        it 'should return an empty list' do
+          spec = {}
+          spec['network_a'] = {'type' => 'dynamic'}
+
+          nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+          expect(nc.security_groups).to eq([])
+        end
+      end
     end
 
-    it "should extract private ip addresses from multiple manual networks" do
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(several_manual_networks)
-      expect(nc.private_ips).to eq(%w[10.0.0.1 10.0.0.2])
-    end
+    context 'when security_groups is not an array' do
+      it 'should raise an ArgumentError' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        set_security_groups(spec['network_a'], 'foo')
 
-    it "should not extract private ip address for dynamic network" do
-      spec = {}
-      spec["network_a"] = dynamic_network_spec
-      spec["network_a"]["ip"] = "10.0.0.1"
-
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.private_ips).to be_empty
-    end
-  end
-
-  describe "nics" do
-    it "should extract net_id from dynamic network" do
-      spec = {}
-      spec["network_a"] = dynamic_network_spec
-      spec["network_a"]["cloud_properties"]["net_id"] = "foo"
-
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.nics).to eq([{"net_id" => "foo"}])
-    end
-
-    it "should extract net_id and IP address from all manual networks" do
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(several_manual_networks)
-      expect(nc.nics).to eq([
-            {"net_id" => "net", "v4_fixed_ip" => "10.0.0.1"},
-            {"net_id" => "bar", "v4_fixed_ip" => "10.0.0.2"},
-          ])
-    end
-
-    it "should not extract ip address for dynamic network" do
-      spec = {}
-      spec["network_a"] = dynamic_network_spec
-      spec["network_a"]["ip"] = "10.0.0.1"
-      spec["network_a"]["cloud_properties"]["net_id"] = "foo"
-
-      nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
-      expect(nc.nics).to eq([{"net_id" => "foo"}])
+        expect {
+          Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        }.to raise_error ArgumentError, 'security groups must be an Array'
+      end
     end
   end
 
-  describe "configure" do
+  describe '#private_ips' do
+    context 'when manual network' do
+      it 'should extract private ip address' do
+        spec = {}
+        spec['network_a'] = manual_network_spec
+        spec['network_a']['ip'] = '10.0.0.1'
+
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.private_ips).to eq(%w[10.0.0.1])
+      end
+
+      context 'when multiple manual networks' do
+        it 'should extract private ip addresses from all' do
+          nc = Bosh::OpenStackCloud::NetworkConfigurator.new(several_manual_networks)
+          expect(nc.private_ips).to eq(%w[10.0.0.1 10.0.0.2])
+        end
+      end
+
+      context 'when additional vip network' do
+        it 'should extract private ip address from manual network' do
+          spec = {}
+          spec['network_a'] = vip_network_spec
+          spec['network_a']['ip'] = '10.0.0.1'
+          spec['network_b'] = manual_network_spec
+          spec['network_b']['ip'] = '10.0.0.2'
+
+          nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+          expect(nc.private_ips).to eq(%w[10.0.0.2])
+        end
+      end
+    end
+
+    context 'when dynamic network' do
+      it 'should not extract private ip address' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        spec['network_a']['ip'] = '10.0.0.1'
+
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.private_ips).to be_empty
+      end
+    end
+  end
+
+  describe '#nics' do
+    context 'when dynamic network' do
+      it 'should extract net_id' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        spec['network_a']['cloud_properties']['net_id'] = 'foo'
+
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.nics).to eq([{'net_id' => 'foo'}])
+      end
+
+      it 'should not extract ip address' do
+        spec = {}
+        spec['network_a'] = dynamic_network_spec
+        spec['network_a']['ip'] = '10.0.0.1'
+        spec['network_a']['cloud_properties']['net_id'] = 'foo'
+
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(spec)
+        expect(nc.nics).to eq([{'net_id' => 'foo'}])
+      end
+    end
+
+    context 'when manual network' do
+      it 'should extract net_id and IP address from all' do
+        nc = Bosh::OpenStackCloud::NetworkConfigurator.new(several_manual_networks)
+        expect(nc.nics).to eq([
+                                  {'net_id' => 'net', 'v4_fixed_ip' => '10.0.0.1'},
+                                  {'net_id' => 'bar', 'v4_fixed_ip' => '10.0.0.2'},
+                              ])
+      end
+    end
+  end
+
+  describe '#configure' do
     let(:vip_network) do
       network = double('vip_network')
       allow(Bosh::OpenStackCloud::VipNetwork).to receive(:new).and_return(network)
@@ -209,7 +234,7 @@ describe Bosh::OpenStackCloud::NetworkConfigurator do
       network
     end
 
-    context "With vip network" do
+    context 'With vip network' do
       let(:network_spec) do
         {
           'network_a' => dynamic_network_spec,
@@ -218,14 +243,8 @@ describe Bosh::OpenStackCloud::NetworkConfigurator do
         }
       end
 
-      it "configures the vip network if it exists" do
+      it 'configures the vip network and all other networks too' do
         expect(vip_network).to receive(:configure)
-        network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
-        network_configurator.configure(nil, nil)
-      end
-
-      it "configures the other networks too" do
-        allow(vip_network).to receive(:configure)
         expect(manual_network).to receive(:configure)
         expect(dynamic_network).to receive(:configure)
         network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
@@ -233,7 +252,7 @@ describe Bosh::OpenStackCloud::NetworkConfigurator do
       end
     end
 
-    context "No vip network" do
+    context 'when no vip network' do
       let(:network_spec) do
         {
           'network_a' => dynamic_network_spec,
@@ -241,51 +260,56 @@ describe Bosh::OpenStackCloud::NetworkConfigurator do
         }
       end
 
-      it "disassociate allocated floating IP" do
-        server = double("server", :id => "i-test")
-        address = double("address", :id => "a-test", :ip => "10.0.0.1",
-          :instance_id => "i-test")
+      context 'when floating IP associated to the server' do
+        it 'disassociate allocated floating IP' do
+          server = double('server', :id => 'i-test')
+          address = double('address', :id => 'a-test', :ip => '10.0.0.1',
+                           :instance_id => 'i-test')
 
-        expect(manual_network).to receive(:configure)
-        expect(dynamic_network).to receive(:configure)
-        expect(vip_network).to_not receive(:configure)
-        cloud = mock_cloud do |openstack|
-          expect(openstack).to receive(:addresses).and_return([address])
+          expect(manual_network).to receive(:configure)
+          expect(dynamic_network).to receive(:configure)
+          expect(vip_network).to_not receive(:configure)
+          cloud = mock_cloud do |openstack|
+            expect(openstack).to receive(:addresses).and_return([address])
+          end
+          expect(address).to receive(:server=).with(nil)
+
+          network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
+          network_configurator.configure(cloud.compute, server)
         end
-        expect(address).to receive(:server=).with(nil)
-
-        network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
-        network_configurator.configure(cloud.compute, server)
       end
 
-      it "floating IPs allocated to other servers" do
-        other_server = double("server", :id => "i-test2")
-        address = double("address", :id => "a-test", :ip => "10.0.0.1",
-          :instance_id => "i-test")
+      context 'when no floating IP associated to the server but to others' do
+        it 'should not disassociate any floating IP' do
+          other_server = double('server', :id => 'i-test2')
+          address = double('address', :id => 'a-test', :ip => '10.0.0.1',
+                           :instance_id => 'i-test')
 
-        expect(manual_network).to receive(:configure)
-        expect(dynamic_network).to receive(:configure)
-        expect(vip_network).to_not receive(:configure)
+          expect(manual_network).to receive(:configure)
+          expect(dynamic_network).to receive(:configure)
+          expect(vip_network).to_not receive(:configure)
 
-        cloud = mock_cloud do |openstack|
-          expect(openstack).to receive(:addresses).and_return([address])
+          cloud = mock_cloud do |openstack|
+            expect(openstack).to receive(:addresses).and_return([address])
+          end
+          expect(address).to_not receive(:server=).with(nil)
+
+          network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
+          network_configurator.configure(cloud.compute, other_server)
         end
-        expect(address).to_not receive(:server=).with(nil)
-
-        network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
-        network_configurator.configure(cloud.compute, other_server)
       end
+      context 'when no floating IP is associated' do
+        it 'should not configure the vip_network' do
+          expect(manual_network).to receive(:configure)
+          expect(dynamic_network).to receive(:configure)
+          expect(vip_network).to_not receive(:configure)
+          cloud = mock_cloud do |openstack|
+            expect(openstack).to receive(:addresses).and_return([])
+          end
 
-      it "no floating IPs allocated" do
-        expect(manual_network).to receive(:configure)
-        expect(dynamic_network).to receive(:configure)
-        expect(vip_network).to_not receive(:configure)
-        cloud = mock_cloud do |openstack|
-          expect(openstack).to receive(:addresses).and_return([])
+          network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
+          network_configurator.configure(cloud.compute, nil)
         end
-
-        network_configurator = Bosh::OpenStackCloud::NetworkConfigurator.new(network_spec)
-        network_configurator.configure(cloud.compute, nil)
       end
     end
   end
