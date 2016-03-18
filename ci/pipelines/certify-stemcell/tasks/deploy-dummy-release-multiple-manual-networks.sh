883b528ba79781ec22d88cc78e0ff74851f9e147
Beyhan Veli <beyhan.veli@sap.com>
2016-03-18 11:28:20 +0100

    Extend 'config_drive' e2e test
    - verify that dummy job vm has both manual network
      interfaces configured
    
    [#114967305](https://www.pivotaltracker.com/story/show/114967305)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- ci/pipelines/certify-stemcell/tasks/deploy-dummy-release-multiple-manual-networks.sh
+++ ci/pipelines/certify-stemcell/tasks/deploy-dummy-release-multiple-manual-networks.sh
@@ -5,13 +5,16 @@ set -e
 source bosh-cpi-src-in/ci/tasks/utils.sh
 
 ensure_not_replace_value bosh_director_ip
+ensure_not_replace_value v3_e2e_private_key_data
 ensure_not_replace_value dns
 ensure_not_replace_value v3_e2e_security_group
 ensure_not_replace_value stemcell_name
 ensure_not_replace_value network_id
+ensure_not_replace_value network_no_dhcp_1_id
 ensure_not_replace_value network_no_dhcp_1_range
 ensure_not_replace_value network_no_dhcp_1_gateway
 ensure_not_replace_value network_no_dhcp_1_ip
+ensure_not_replace_value network_no_dhcp_2_id
 ensure_not_replace_value network_no_dhcp_2_range
 ensure_not_replace_value network_no_dhcp_2_gateway
 ensure_not_replace_value network_no_dhcp_2_ip
@@ -83,7 +86,7 @@ networks:
         dns:     ${dns}
         static:  [${network_no_dhcp_1_ip}]
         cloud_properties:
-          net_id: ${network_id}
+          net_id: ${network_no_dhcp_1_id}
           security_groups: [${v3_e2e_security_group}]
 
   - name: manual-2
@@ -94,7 +97,7 @@ networks:
         dns:     ${dns}
         static:  [${network_no_dhcp_2_ip}]
         cloud_properties:
-          net_id: ${network_id}
+          net_id: ${network_no_dhcp_2_id}
           security_groups: [${v3_e2e_security_group}]
 
 jobs:
@@ -125,6 +128,23 @@ pushd ${deployment_dir}
   echo "deploying dummy release..."
   bosh deployment ${manifest_filename}
   bosh -n deploy
+  echo "checking network interfaces..."
+  echo "${v3_e2e_private_key_data}" > bosh.pem
+  chmod go-r bosh.pem
+  bosh ssh --gateway_host ${bosh_director_ip} --gateway_user vcap --gateway_identity_file bosh.pem dummy 0 ifconfig > network_config
+
+  cat network_config | grep ${network_no_dhcp_1_ip} || failed_exit_code_1=$?
+  if [ $failed_exit_code_1 ]; then
+    echo "failed to find network interface with ip: " ${network_no_dhcp_1_ip}
+    exit 1
+  fi
+
+  cat network_config | grep ${network_no_dhcp_2_ip} || failed_exit_code_2=$?
+  if [ $failed_exit_code_2 ]; then
+    echo "failed to find network interface with ip: " ${network_no_dhcp_2_ip}
+    exit 1
+  fi
+
   if [ "${delete_deployment_when_done}" = "true" ]; then
     bosh -n delete deployment dummy
     bosh -n cleanup --all
