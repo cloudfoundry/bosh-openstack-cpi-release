a80a313e050c20382dc1e0c6eca3860d146ab3c2
Matthias Winzeler <matthias.winzeler@swisscom.com>
2016-08-17 16:46:01 +0200

    multi-cpi: next try to make cert handling cleaner

--- src/bosh_openstack_cpi/lib/cloud/openstack/cpi_lambda.rb
+++ src/bosh_openstack_cpi/lib/cloud/openstack/cpi_lambda.rb
@@ -9,36 +9,28 @@ module Bosh::OpenStackCloud
         end
 
         cloud_properties = cpi_config['cloud']['properties']
-
         cloud_properties['cpi_log'] = cpi_log
 
-        # If 'ca_cert' is set we render non-empty `config/openstack.crt`
-        set_ca_cert_in_connection_options(cloud_properties, ca_cert_from_config)
+        # If 'ca_cert' is set in job config we render non-empty `config/openstack.crt` (excon needs it as a file)
+        connection_options = cloud_properties['openstack']['connection_options']
+        if connection_options && connection_options.delete('ca_cert')
+          connection_options['ssl_ca_file'] = ca_cert_from_config
+        end
 
         # allow openstack config to be overwritten dynamically by context
         if context && context['cpi_properties']
           cloud_properties['openstack'] = context['cpi_properties']
 
-          write_ca_cert_to_disk(cloud_properties, ca_cert_from_context)
-          set_ca_cert_in_connection_options(cloud_properties, ca_cert_from_context)
+          # write ca cert to disk if given in context
+          connection_options = cloud_properties['openstack']['connection_options']
+          if connection_options && (ca_cert = connection_options.delete('ca_cert'))
+            File.write(ca_cert_from_context, ca_cert)
+            connection_options['ssl_ca_file'] = ca_cert_from_context
+          end
         end
 
         Bosh::Clouds::Openstack.new(cloud_properties)
       end
-    end
-
-    private
-    def self.set_ca_cert_in_connection_options(cloud_properties, ca_cert_path)
-      connection_options = cloud_properties['openstack']['connection_options']
-      if connection_options && connection_options['ca_cert']
-        connection_options['ssl_ca_file'] = ca_cert_path
-        connection_options.delete('ca_cert')
-      end
-    end
-
-    def self.write_ca_cert_to_disk(cloud_properties, ca_cert_path)
-      connection_options = cloud_properties['openstack']['connection_options']
-      File.write(ca_cert_path, connection_options['ca_cert']) if connection_options && connection_options['ca_cert']
     end
   end
 end
