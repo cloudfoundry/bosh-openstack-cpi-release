7304e30c8c7fdd08c2251ca2a0bfd81e3cda4be9
Matthias Winzeler <matthias.winzeler@swisscom.com>
2016-08-17 11:37:34 +0200

    multi-cpi: separating writing ca file to disk from setting it in options

--- src/bosh_openstack_cpi/lib/cloud/openstack/cpi_lambda.rb
+++ src/bosh_openstack_cpi/lib/cloud/openstack/cpi_lambda.rb
@@ -18,7 +18,9 @@ module Bosh::OpenStackCloud
         # allow openstack config to be overwritten dynamically by context
         if context && context['cpi_properties']
           cloud_properties['openstack'] = context['cpi_properties']
-          set_ca_cert_in_connection_options(cloud_properties, ca_cert_from_context, true)
+
+          write_ca_cert_to_disk(cloud_properties, ca_cert_from_context)
+          set_ca_cert_in_connection_options(cloud_properties, ca_cert_from_context)
         end
 
         Bosh::Clouds::Openstack.new(cloud_properties)
@@ -26,13 +28,17 @@ module Bosh::OpenStackCloud
     end
 
     private
-    def self.set_ca_cert_in_connection_options(cloud_properties, ca_cert_path, write_to_disk=false)
+    def self.set_ca_cert_in_connection_options(cloud_properties, ca_cert_path)
       connection_options = cloud_properties['openstack']['connection_options']
       if connection_options && connection_options['ca_cert']
-        File.write(ca_cert_path, connection_options['ca_cert']) if write_to_disk
         connection_options['ssl_ca_file'] = ca_cert_path
         connection_options.delete('ca_cert')
       end
+    end
+
+    def self.write_ca_cert_to_disk(cloud_properties, ca_cert_path)
+      connection_options = cloud_properties['openstack']['connection_options']
+      File.write(ca_cert_path, connection_options['ca_cert']) if connection_options && connection_options['ca_cert']
     end
   end
 end
