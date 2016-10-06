1eb3bd32703a573df3c94508d402212dd8bac52f
Mauro Morales <mamorales@suse.com>
2016-10-06 10:39:25 +0200

    Print tasks in error after BATs run
    - use correct director ip
    - print last 100 tasks before creating the debug commands
    
    [#126038495](https://www.pivotaltracker.com/story/show/126038495)
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- ci/ruby_scripts/print_task_debug_output.sh
+++ ci/ruby_scripts/print_task_debug_output.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 require_relative 'failed_tasks_parser'
 
-cmds = errors_to_bosh_tasks_cmds(ARGF.read)
+tasks = ARGF.read
+
+puts tasks
+
+cmds = errors_to_bosh_tasks_cmds(tasks)
 cmds.each do |cmd|
   puts `#{cmd}`
 end
--- ci/tasks/print_task_errors.sh
+++ ci/tasks/print_task_errors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x -e
+set -e
 
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
@@ -13,4 +13,4 @@ echo 'Printing debug output of tasks in state error'
 cd bosh-cpi-src-in/ci/ruby_scripts
 bosh -n target ${bosh_director_public_ip}
 bosh -n login admin ${bosh_admin_password}
-bosh -n tasks recent --no-filter 100 | ./print_task_debug_output.rb
+bosh -n tasks recent --no-filter 100 | ./print_task_debug_output.sh
