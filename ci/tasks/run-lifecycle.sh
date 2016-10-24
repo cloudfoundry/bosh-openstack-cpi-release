550d13798456a253713998eb6fb759a0e8fb722a
Felix Riegger <felix.riegger@sap.com>
2016-10-24 13:43:54 +0200

    Apply lifecycle terraform before running lifecycle tests
    [#119565681](https://www.pivotaltracker.com/story/show/119565681)
    
    Signed-off-by: Mauro Morales <mamorales@suse.com>

--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -8,12 +8,6 @@ set -o pipefail
 : ${BOSH_OPENSTACK_USERNAME_V3:?}
 : ${BOSH_OPENSTACK_API_KEY_V3:?}
 : ${BOSH_OPENSTACK_PROJECT:?}
-: ${BOSH_OPENSTACK_MANUAL_IP:?}
-: ${BOSH_OPENSTACK_NO_DHCP_MANUAL_IP_1:?}
-: ${BOSH_OPENSTACK_NO_DHCP_MANUAL_IP_2:?}
-: ${BOSH_OPENSTACK_NET_ID:?}
-: ${BOSH_OPENSTACK_NET_ID_NO_DHCP_1:?}
-: ${BOSH_OPENSTACK_NET_ID_NO_DHCP_2:?}
 : ${BOSH_OPENSTACK_DEFAULT_KEY_NAME:?}
 : ${BOSH_CLI_SILENCE_SLOW_LOAD_WARNING:?}
 : ${BOSH_OPENSTACK_CONNECT_TIMEOUT:?}
@@ -28,6 +22,15 @@ set -o pipefail
 : ${BOSH_OPENSTACK_VOLUME_TYPE:-""}
 
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
+
+metadata=terraform-lifecycle/metadata
+
+export BOSH_OPENSTACK_MANUAL_IP=$(cat ${metadata} | jq --raw-output ".lifecycle_manual_ip")
+export BOSH_OPENSTACK_NO_DHCP_MANUAL_IP_1=$(cat ${metadata} | jq --raw-output ".lifecycle_no_dhcp_manual_ip_1")
+export BOSH_OPENSTACK_NO_DHCP_MANUAL_IP_2=$(cat ${metadata} | jq --raw-output ".lifecycle_no_dhcp_manual_ip_2")
+export BOSH_OPENSTACK_NET_ID=$(cat ${metadata} | jq --raw-output ".lifecycle_openstack_net_id")
+export BOSH_OPENSTACK_NET_ID_NO_DHCP_1=$(cat ${metadata} | jq --raw-output ".lifecycle_net_id_no_dhcp_1")
+export BOSH_OPENSTACK_NET_ID_NO_DHCP_2=$(cat ${metadata} | jq --raw-output ".lifecycle_net_id_no_dhcp_2")
 
 mkdir "${PWD}/openstack-lifecycle-stemcell/stemcell"
 tar -C "${PWD}/openstack-lifecycle-stemcell/stemcell" -xzf "${PWD}/openstack-lifecycle-stemcell/stemcell.tgz"
