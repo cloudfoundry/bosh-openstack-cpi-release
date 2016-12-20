41ded96bfcf8e64ead7877c2415f3085f2ce21b8
Matthias Winzeler <matthias.winzeler@gmail.com>
2016-12-20 13:49:54 +0100

    multi CPI support (#52)
    * multi cpi: allowing openstack properties to by overwritten by context (refactored cpi lambda to test this)
    
    * multi-cpi: supporting ca_cert for context properties
    
    * multi-cpi: separating writing ca file to disk from setting it in options
    
    * multi-cpi: next try to make cert handling cleaner
    
    * multi-cpi: bosh_cpi now merges cpi properties in context so we can use context['openstack']
    
    * multi-cpi: don't just overwrite openstack properties but merge, so that defaults from bosh release spec are respected
    
    * multi-cpi: no need to check for context.nil? since it's always provided as not-nil by bosh
    
    * multi-cpi: updating upstream bosh gems to contain context passing
    
    * multi-cpi: vendored updated upstream bosh gems
    
    * multi-cpi: cpi properties are no longer in openstack subkey of context, but in toplevel
    
    * Fix context cacert handling
    
    Vcap user is not allowed to write to
    `/var/vcap/jobs/openstack_cpi/config/cacert_context.pem`.
    Instead of using this fixed path, a tmpdir is created for each cpi
    call. The lambda writes the cacert into this tmpdir. The tmpdir is
    deleted after the cpi call has finished.
    
    [#134693605](https://www.pivotaltracker.com/story/show/134693605)
    
    Signed-off-by: Jan von Loewenstein <jan.von.loewenstein@sap.com>

---
+++ src/bosh_openstack_cpi/spec/unit/cpi_lambda_spec.rb
@@ -0,0 +1,90 @@
+require "spec_helper"
+
+describe Bosh::OpenStackCloud::CpiLambda do
+  subject { described_class.create(cpi_config, cpi_log, ssl_ca_file, ca_cert_from_context) }
+  let(:cpi_config) {
+    {
+        'cloud' => {
+            'properties' => {
+                'openstack' => {
+                    'key1' => 'value1',
+                    'key2' => 'value2'
+                }
+            }
+        }
+    }
+  }
+  let(:ssl_ca_file) { 'feel-free-to-change' }
+  let(:cpi_log) { StringIO.new }
+  let(:ca_cert_from_context) { Tempfile.new('ca_cert').path }
+
+  describe 'when creating a cloud' do
+    it 'passes parts of the cpi config to openstack' do
+      expect(Bosh::Clouds::Openstack).to receive(:new).with({'openstack' => cpi_config['cloud']['properties']['openstack'],
+                                                             'cpi_log' => cpi_log})
+      subject.call({})
+    end
+
+    context 'if invalid cpi config is given' do
+      let(:cpi_config) {{'empty' => 'config'}}
+
+      it 'raises an error' do
+        expect {
+          subject.call({})
+        }.to raise_error /Could not find cloud properties in the configuration/
+      end
+    end
+
+    context 'if using ca_certs in config' do
+      let(:cpi_config) {{ 'cloud' => {'properties' => { 'openstack' => {'connection_options' => {'ca_cert' => 'xyz'}}}}}}
+
+      it 'sets ssl_ca_file that is passed and removes ca_certs' do
+        expect(Bosh::Clouds::Openstack).to receive(:new).with({'openstack' => {'connection_options' => {'ssl_ca_file' => ssl_ca_file}},
+                                                               'cpi_log' => cpi_log})
+        subject.call({})
+      end
+    end
+
+    context 'if openstack properties are provided in the context' do
+      it 'merges the openstack properties' do
+        context = {
+            'newkey' => 'newvalue',
+            'newkey2' => 'newvalue2',
+        }
+
+        expect(Bosh::Clouds::Openstack).to receive(:new).with({'openstack' => { 'key1' => 'value1',
+                                                                                'key2' => 'value2',
+                                                                                'newkey' => 'newvalue',
+                                                                                'newkey2' => 'newvalue2'},
+                                                               'cpi_log' => cpi_log})
+        subject.call(context)
+      end
+
+      it 'writes the given ca_cert to the disk and sets ssl_ca_file to its path' do
+        context = {
+            'newkey' => 'newvalue',
+            'connection_options' => {'ca_cert' => 'xyz'}
+        }
+
+        expect(Bosh::Clouds::Openstack).to receive(:new).with({'openstack' => { 'newkey' => 'newvalue',
+                                                                                'key1' => 'value1',
+                                                                                'key2' => 'value2',
+                                                                                'connection_options' => {'ssl_ca_file' => ca_cert_from_context}},
+                                                               'cpi_log' => cpi_log})
+
+        subject.call(context)
+        expect(File.read(ca_cert_from_context)).to eq('xyz')
+      end
+
+      context 'when the context does not include a ca_cert' do
+        it 'does not write into the file' do
+          allow(Bosh::Clouds::Openstack).to receive(:new)
+
+          subject.call({})
+
+          expect(File.read(ca_cert_from_context)).to eq('')
+        end
+      end
+    end
+  end
+end
