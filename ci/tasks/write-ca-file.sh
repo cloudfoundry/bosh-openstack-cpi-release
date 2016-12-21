44f9953f13bdcc2144e269c56460b14030266b0e
Beyhan Veli <beyhan.veli@sap.com>
2016-12-21 11:28:04 +0100

    Make terraform execution in CI without certificate possible
    [#133990233](https://www.pivotaltracker.com/story/show/133990233)

--- ci/tasks/write-ca-file.sh
+++ ci/tasks/write-ca-file.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-: ${file_content:?}
+optional_value file_content
 
 mkdir -p write-file
 echo "$(echo ${file_content} | sed -r  -e 's/ /\n/g ' -e 's/\nCERTIFICATE-----/ CERTIFICATE-----/g')" > write-file/file
