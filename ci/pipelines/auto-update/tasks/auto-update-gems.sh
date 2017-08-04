272a91abf07b67c38e761fd57e1942a47f7ce0b8
Mauro Morales <mamorales@suse.com>
2017-08-04 16:28:29 +0200

    Create PR in pipeline task
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- ci/pipelines/auto-update/tasks/auto-update-gems.sh
+++ ci/pipelines/auto-update/tasks/auto-update-gems.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+echo "Check if PR is open on auto-update branch"
+#pr_open=$(git branch master --contains $(git rev-parse origin/auto-update))
+pr_open=$(git branch master --contains $(git rev-parse origin/master))
+if [ -z ${pr_open} ]; then
+  echo "PR is open. Merge first"
+  exit 1
+fi
+
 #cd bosh-cpi-src-in
 pushd src/bosh_openstack_cpi
   echo "Looking for new gem versions"
@@ -15,11 +23,12 @@ pushd src/bosh_openstack_cpi
     bundle exec rspec spec/unit/*
 
     echo "Creating new pull request"
-#      git add .
-#      git config --global user.email cf-bosh-eng@pivotal.io
-#      git config --global user.name CI
-#      git commit -m "Bump gems"
-#      git push origin HEAD:hub
+      git add .
+      git config --global user.email cf-bosh-eng@pivotal.io
+      git config --global user.name CI
+      git commit -m "Bump gems"
+      git push origin HEAD:auto-update-test
+      hub pull-request -b origin:master -h origin:auto-update-test -m "Bump gems"
   else
     echo "No new gem versions found"
   fi
