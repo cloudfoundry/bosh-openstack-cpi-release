9e855c6e4a80defd127942afb65d887c645c2d8a
Beyhan Veli <beyhan.veli@sap.com>
2017-08-07 17:25:08 +0200

    Fix task script
    [#149794797](https://www.pivotaltracker.com/story/show/149794797)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- ci/pipelines/auto-update/tasks/auto-update-gems.sh
+++ ci/pipelines/auto-update/tasks/auto-update-gems.sh
@@ -3,32 +3,32 @@
 set -e
 set -x
 
-echo "Check if latest auto-update commit has already been merged to master"
-pr_open=$(git branch master --contains $(git rev-parse origin/auto-update))
-if [ -z ${pr_open} ]; then
-  echo "PR is open. Merge first"
-  exit 1
-fi
+pushd bosh-cpi-src-in
+  echo "Check if latest auto-update commit has already been merged to master"
+  pr_open=$(git branch master --contains $(git rev-parse origin/auto-update))
+  if [ -z ${pr_open} ]; then
+    echo "PR is open. Merge first"
+    exit 1
+  fi
+popd
 
 cp -r bosh-cpi-src-in bosh-cpi-src-out
 
-cd bosh-cpi-src-out
-pushd src/bosh_openstack_cpi
-  echo "Looking for new gem versions"
-  rm Gemfile.lock
-  ./vendor_gems
-  git diff --exit-code Gemfile.lock || exit_code=$?
-  if [ -v exit_code ]; then
-    echo "Running unit tests"
-    bundle install
-    bundle exec rspec spec/unit/*
+cd bosh-cpi-src-out/src/bosh_openstack_cpi
+echo "Looking for new gem versions"
+rm Gemfile.lock
+./vendor_gems
+git diff --exit-code Gemfile.lock || exit_code=$?
+if [ -v exit_code ]; then
+echo "Running unit tests"
+bundle install
+bundle exec rspec spec/unit/*
 
-    echo "Creating new pull request"
-      git add .
-      git config --global user.email cf-bosh-eng@pivotal.io
-      git config --global user.name CI
-      git commit -m "Bump gems"
-  else
-    echo "No new gem versions found"
-  fi
-popd
+echo "Creating new pull request"
+  git add .
+  git config --global user.email cf-bosh-eng@pivotal.io
+  git config --global user.name CI
+  git commit -m "Bump gems"
+else
+echo "No new gem versions found"
+fi
