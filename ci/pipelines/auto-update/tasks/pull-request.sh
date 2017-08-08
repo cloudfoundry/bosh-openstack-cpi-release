0b83cff1fa3d48179ff0dcf4849c315e6bdbd028
Tom Kiemes <tom.kiemes@sap.com>
2017-08-08 15:23:37 +0200

    Change handling of changes in aut-update pipeline
    [#149794797](https://www.pivotaltracker.com/story/show/149794797)
    
    Signed-off-by: Beyhan Veli <beyhan.veli@sap.com>

--- ci/pipelines/auto-update/tasks/pull-request.sh
+++ ci/pipelines/auto-update/tasks/pull-request.sh
@@ -3,10 +3,25 @@
 set -e
 set -x
 
+export GITHUB_TOKEN=${bosh-openstack-cpi-release-github-token}
+export SSH_KEY="ssh.key"
+echo "${bosh-openstack-cpi-release-github-key}" > ${SSH_KEY}
+eval $(ssh-agent)
+chmod go-r ${SSH_KEY}
+ssh-add ${SSH_KEY}
+
+cd bosh-cpi-src-out
 echo "Check if latest auto-update commit has already been merged to master"
-pr_open=$(git branch master --contains $(git rev-parse origin/auto-update))
-if [ -z ${pr_open} ]; then
-  echo "Creating pull-request"
-  cd bosh-cpi-src-out
-  hub pull-request -b master -h auto-update-m "Bump gems"
+git fetch origin master:refs/remotes/origin/master
+new_commits_available=$(git branch master --contains $(git rev-parse origin/auto-update))
+pull_request=$(hub issue | grep "Bump gems") || no_pull_request=$?
+if [ -z ${new_commits_available} ]; then
+  if [ -v no_pull_request ]; then
+    echo "Creating pull-request"
+    hub pull-request -b master -h auto-update -m "Bump gems"
+  else
+    echo "Open pull-request found: ${pull_request}"
+  fi
+else
+  echo "No new commits available"
 fi
