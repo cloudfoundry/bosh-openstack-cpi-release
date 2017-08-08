d24fcd1946cc8f1d843838951b5e62ad14849426
Beyhan Veli <beyhan.veli@sap.com>
2017-08-08 15:26:28 +0200

    Always push to auto-update branch even if PR exists
    [#149794797](https://www.pivotaltracker.com/story/show/149794797)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- ci/pipelines/auto-update/tasks/auto-update-gems.sh
+++ ci/pipelines/auto-update/tasks/auto-update-gems.sh
@@ -3,16 +3,6 @@
 set -e
 set -x
 
-pushd bosh-cpi-src-in
-  echo "Check if latest auto-update commit has already been merged to master"
-  git fetch origin auto-update:refs/remotes/origin/auto-update
-  pr_open=$(git branch master --contains $(git rev-parse origin/auto-update))
-  if [ -z ${pr_open} ]; then
-    echo "PR is open. Merge first"
-    exit 1
-  fi
-popd
-
 cp -r bosh-cpi-src-in bosh-cpi-src-out/repo
 
 cd bosh-cpi-src-out/repo/src/bosh_openstack_cpi
