1e1bb80ca583e8a5669e1063d07c9f581984e521
Beyhan Veli <beyhan.veli@sap.com>
2017-09-15 14:16:59 +0200

    Remove focused tag
    [#150708607](https://www.pivotaltracker.com/story/show/150708607)

--- src/bosh_openstack_cpi/spec/integration/lifecycle_spec.rb
+++ src/bosh_openstack_cpi/spec/integration/lifecycle_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper'
 
-fdescribe Bosh::OpenStackCloud::Cloud do
+describe Bosh::OpenStackCloud::Cloud do
   # @formatter:off
   before(:all) do
     @config = IntegrationConfig.new
