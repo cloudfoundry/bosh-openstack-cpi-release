2f852c9d014919e4b17a2059abf4d92a8e72c7dc
Mario Manno <mario.manno@suse.com>
2018-04-04 15:33:21 +0200

    Make private class methods private and remove unused method
    [#134167789](https://www.pivotaltracker.com/story/show/134167789)

--- src/bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
+++ src/bosh_openstack_cpi/lib/cloud/openstack/excon_logging_instrumentor.rb
@@ -2,47 +2,46 @@ module Bosh::OpenStackCloud
   class ExconLoggingInstrumentor
     REDACTED = '<redacted>'.freeze
 
-    def self.instrument(name, params = {})
-      redacted_params = redact(params)
+    class << self
+      def instrument(name, params = {})
+        redacted_params = redact(params)
 
-      Bosh::Clouds::Config.logger.debug("#{name} #{redacted_params}")
-      yield if block_given?
-    end
-
-    def self.redact(params)
-      redacted_params = params.dup
-      redact_body(redacted_params, 'auth.passwordCredentials.password')
-      redact_body(redacted_params, 'server.user_data')
-      redact_body(redacted_params, 'auth.identity.password.user.password')
-      redact_headers(redacted_params, 'X-Auth-Token')
-      redacted_params
-    end
-
-    private
-
-    def self.redact_body(params, json_path)
-      return unless params.key?(:body) && params[:body].is_a?(String)
-      return unless params.key?(:headers) && params[:headers]['Content-Type'] == 'application/json'
-
-      begin
-        json_content = JSON.parse(params[:body])
-      rescue JSON::ParserError
-        return
+        Bosh::Clouds::Config.logger.debug("#{name} #{redacted_params}")
+        yield if block_given?
       end
-      json_content = Bosh::Cpi::Redactor.redact!(json_content, json_path)
-      params[:body] = JSON.dump(json_content)
-    end
 
-    def self.redact_headers(params, property)
-      return unless params.key?(:headers)
+      def redact(params)
+        redacted_params = params.dup
+        redact_body(redacted_params, 'auth.passwordCredentials.password')
+        redact_body(redacted_params, 'server.user_data')
+        redact_body(redacted_params, 'auth.identity.password.user.password')
+        redact_headers(redacted_params, 'X-Auth-Token')
+        redacted_params
+      end
 
-      headers = params[:headers] = params[:headers].dup
+      private
 
-      headers.store(property, REDACTED)
-    end
+      def redact_body(params, json_path)
+        return unless params.key?(:body) && params[:body].is_a?(String)
+        return unless params.key?(:headers) && params[:headers]['Content-Type'] == 'application/json'
 
-    def self.fetch_property
-      ->(hash, property) { hash.fetch(property, {}) }
+        begin
+          json_content = JSON.parse(params[:body])
+        rescue JSON::ParserError
+          return
+        end
+        json_content = Bosh::Cpi::Redactor.redact!(json_content, json_path)
+        params[:body] = JSON.dump(json_content)
+      end
+
+      def redact_headers(params, property)
+        return unless params.key?(:headers)
+
+        headers = params[:headers] = params[:headers].dup
+
+        headers.store(property, REDACTED)
+      end
+
     end
   end
 end
