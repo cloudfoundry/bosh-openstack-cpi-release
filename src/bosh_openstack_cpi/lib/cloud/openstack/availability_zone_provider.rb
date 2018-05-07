217fa3fdcfc456ed0ef641097c7ab76fd41294e7
Beyhan Veli <beyhan.veli@sap.com>
2018-05-07 16:53:20 +0200

    Refactor availability zone selection
    [#156221894](https://www.pivotaltracker.com/story/show/156221894)
    
    Co-authored-by: Max Becker <max.becker@sap.com>

--- src/bosh_openstack_cpi/lib/cloud/openstack/availability_zone_provider.rb
+++ src/bosh_openstack_cpi/lib/cloud/openstack/availability_zone_provider.rb
@@ -8,10 +8,15 @@ module Bosh::OpenStackCloud
     end
 
     def select(volume_ids, resource_pool_az)
-      if volume_ids_not_empty?(volume_ids) && use_server_availability_zone?
-        ensure_same_availability_zone(volume_ids, resource_pool_az)
+      return resource_pool_az if volume_ids_empty?(volume_ids) || @ignore_server_availability_zone
+
+      volumes = volumes(volume_ids)
+      volume_azs = volumes.map(&:availability_zone)
+      if resource_pool_az
+        all_azs = volume_azs + [resource_pool_az]
+        same_availability_zone?(all_azs) ? resource_pool_az : fail_for_different_azs(resource_pool_az, volumes)
       else
-        resource_pool_az
+        same_availability_zone?(volume_azs) ? first_volume_az(volume_azs): fail_for_different_azs(resource_pool_az, volumes)
       end
     end
 
@@ -21,24 +26,28 @@ module Bosh::OpenStackCloud
 
     private
 
-    def volume_ids_not_empty?(volume_ids)
-      volume_ids && !volume_ids.empty?
+    def first_volume_az(volume_azs)
+      volume_azs.first.nil? || volume_azs.first.empty? ? nil : volume_azs.first
     end
 
-    def ensure_same_availability_zone(volume_ids, resource_pool_az)
-      fog_volume_map = @openstack.volume.volumes
-      volumes = volume_ids.map { |vid| @openstack.with_openstack { fog_volume_map.get(vid) } }
-      azs = volumes.map(&:availability_zone)
-      azs << resource_pool_az if resource_pool_az
+    def fail_for_different_azs(resource_pool_az, volumes)
+      cloud_error format("can't use multiple availability zones: %s, %s. " +
+          "Enable 'openstack.ignore_server_availability_zone' to allow VMs and disks to be in different AZs, or use the same AZ for both.",
+          resource_pool_az_description(resource_pool_az), disk_az_description(volumes))
+    end
 
+    def volume_ids_empty?(volume_ids)
+      volume_ids.nil? || volume_ids.empty?
+    end
+
+    def same_availability_zone?(azs)
       uniq_azs = azs.uniq
-      if uniq_azs.size > 1
-        cloud_error format("can't use multiple availability zones: %s, %s. " +
-                           "Enable 'openstack.ignore_server_availability_zone' to allow VMs and disks to be in different AZs, or use the same AZ for both.",
-                           resource_pool_az_description(resource_pool_az), disk_az_description(volumes))
-      else
-        uniq_azs.first.nil? || uniq_azs.first.empty? ? nil : uniq_azs.first
-      end
+      uniq_azs.size == 1
+    end
+
+    def volumes(volume_ids)
+      fog_volume_map = @openstack.volume.volumes
+      volume_ids.map {|vid| @openstack.with_openstack {fog_volume_map.get(vid)}}
     end
 
     def resource_pool_az_description(resource_pool_az)
