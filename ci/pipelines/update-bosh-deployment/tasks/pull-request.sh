f8588de2163cb0006ed2c537b9584c57e2bc2f07
Kai Hofstetter <kai.hofstetter@sap.com>
2018-07-19 15:49:36 +0200

    bosh-deployment update pipeline
    [#149725227](https://www.pivotaltracker.com/story/show/149725227)
    
    Co-authored-by: Manuel Dewald <m.dewald@sap.com>

---
+++ ci/pipelines/update-bosh-deployment/tasks/pull-request.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+export GITHUB_TOKEN=${github_token}
+
+pushd ${head_repo}
+  head_commit=$(git rev-parse origin/${head_branch})
+popd
+
+cd ${base_repo}
+echo "Check if latest auto-update commit has already been merged to ${base_branch}"
+git fetch origin ${base_branch}:refs/remotes/origin/${base_branch}
+git checkout ${base_branch}
+
+no_new_commits_available=$(git cat-file -e ${head_commit} ; echo $?)
+pull_request=$(hub issue | grep "${pr_message}") || no_pull_request=$?
+
+if [[ "${no_new_commits_available}" -eq "0" ]]; then
+  echo "No new commits available"
+else
+  if [ -v no_pull_request ]; then
+    echo "Creating pull-request"
+    hub pull-request -b ${base_branch} -h ${head_owner}:${head_branch} -m "${pr_message}"
+  else
+    echo "Open pull-request found: ${pull_request}"
+  fi
+fi
