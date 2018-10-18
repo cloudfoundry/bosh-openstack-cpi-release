9e1f6894d0ef7e864f704a70cfdf4f7cb3c5be7d
Danny Berger <dberger@pivotal.io>
2018-10-18 15:29:08 +0200

    Refactor template tests
    * use bosh-template gem
    
    Co-authored-by: Manuel Dewald <m.dewald@sap.com>

--- src/bosh_openstack_cpi/spec/unit/bosh_release/jobs/cpi/templates/cpi.json.erb_spec.rb
+++ src/bosh_openstack_cpi/spec/unit/bosh_release/jobs/cpi/templates/cpi.json.erb_spec.rb
@@ -1,48 +1,41 @@
 require 'spec_helper'
 require 'json'
 require 'yaml'
+require 'bosh/template/test'
 
 describe 'cpi.json.erb' do
-  let(:cpi_specification_file) { File.absolute_path(File.join(jobs_root, 'openstack_cpi/spec')) }
+  subject { JSON.parse(template.render(manifest)) }
 
-  subject(:parsed_json) do
-    context_hash = YAML.load_file(cpi_specification_file)
-    context = TemplateEvaluationContext.new(context_hash, manifest)
-    renderer = ERBRenderer.new(context)
-    parsed_json = JSON.parse(renderer.render(cpi_json_erb))
-    parsed_json
-  end
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../../../../../../../../')) }
+  let(:job) { release.job('openstack_cpi') }
+  let(:template) { job.template('config/cpi.json') }
 
-  let(:jobs_root) { File.join(File.dirname(__FILE__), '../../../../../../../../', 'jobs') }
-  let(:cpi_json_erb) { File.read(File.absolute_path(File.join(jobs_root, 'openstack_cpi/templates/cpi.json.erb'))) }
   let(:manifest) do
     {
-      'properties' => {
-        'openstack' => {
-          'auth_url' => 'openstack.auth_url',
-          'username' => 'openstack.username',
-          'api_key' => 'openstack.api_key',
-          'tenant' => 'openstack.tenant',
-          'default_key_name' => 'openstack.default_key_name',
-          'default_security_groups' => 'openstack.default_security_groups',
-          'wait_resource_poll_interval' => 'openstack.wait_resource_poll_interval',
-          'human_readable_vm_names' => false,
-          'ignore_server_availability_zone' => 'openstack.ignore_server_availability_zone',
-        },
-        'blobstore' => {
-          'provider' => 'local',
-          'path' => 'blobstore-local-path',
-        },
-        'registry' => {
-          'username' => 'registry.username',
-          'password' => 'registry.password',
-          'host' => 'registry.host',
-        },
-        'nats' => {
-          'address' => 'nats_address.example.com',
-          'password' => 'nats-password',
-          'user' => 'nats-user',
-        },
+      'openstack' => {
+        'auth_url' => 'openstack.auth_url',
+        'username' => 'openstack.username',
+        'api_key' => 'openstack.api_key',
+        'tenant' => 'openstack.tenant',
+        'default_key_name' => 'openstack.default_key_name',
+        'default_security_groups' => 'openstack.default_security_groups',
+        'wait_resource_poll_interval' => 'openstack.wait_resource_poll_interval',
+        'human_readable_vm_names' => false,
+        'ignore_server_availability_zone' => 'openstack.ignore_server_availability_zone',
+      },
+      'blobstore' => {
+        'provider' => 'local',
+        'path' => 'blobstore-local-path',
+      },
+      'registry' => {
+        'username' => 'registry.username',
+        'password' => 'registry.password',
+        'host' => 'registry.host',
+      },
+      'nats' => {
+        'address' => 'nats_address.example.com',
+        'password' => 'nats-password',
+        'user' => 'nats-user',
       },
     }
   end
@@ -96,7 +89,7 @@ describe 'cpi.json.erb' do
 
     context 'when provided a minimal configuration' do
       before do
-        manifest['properties']['blobstore'].merge!(
+        manifest['blobstore'].merge!(
           'provider' => 's3',
           'bucket_name' => 'my_bucket',
           'access_key_id' => 'blobstore-access-key-id',
@@ -121,7 +114,7 @@ describe 'cpi.json.erb' do
 
     context 'when provided a maximal configuration' do
       before do
-        manifest['properties']['blobstore'].merge!(
+        manifest['blobstore'].merge!(
           'provider' => 's3',
           'bucket_name' => 'my_bucket',
           'access_key_id' => 'blobstore-access-key-id',
@@ -153,7 +146,7 @@ describe 'cpi.json.erb' do
       end
 
       it 'prefers the agent properties when they are both included' do
-        manifest['properties']['agent'] = {
+        manifest['agent'] = {
           'blobstore' => {
             'access_key_id' => 'agent_access_key_id',
             'secret_access_key' => 'agent_secret_access_key',
@@ -166,7 +159,7 @@ describe 'cpi.json.erb' do
           },
         }
 
-        manifest['properties']['blobstore'].merge!(
+        manifest['blobstore'].merge!(
           'access_key_id' => 'blobstore_access_key_id',
           'secret_access_key' => 'blobstore_secret_access_key',
           's3_region' => 'blobstore-region',
@@ -191,8 +184,8 @@ describe 'cpi.json.erb' do
 
   context 'when using human readable VM names' do
     it 'template render fails if registry endpoint is not set' do
-      manifest['properties']['registry']['endpoint'] = nil
-      manifest['properties']['openstack']['human_readable_vm_names'] = true
+      manifest['registry']['endpoint'] = nil
+      manifest['openstack']['human_readable_vm_names'] = true
 
       expect { subject }.to raise_error RuntimeError,
                                         "Property 'human_readable_vm_names' can only be used together with" \
@@ -200,18 +193,18 @@ describe 'cpi.json.erb' do
     end
 
     it 'template render succeeds if registry endpoint is set' do
-      manifest['properties']['registry']['endpoint'] = 'http://registry.host:25777'
-      manifest['properties']['openstack']['human_readable_vm_names'] = true
+      manifest['registry']['endpoint'] = 'http://registry.host:25777'
+      manifest['openstack']['human_readable_vm_names'] = true
 
       expect(subject['cloud']['properties']['registry']['endpoint']).to eq('http://registry.host:25777')
       expect(subject['cloud']['properties']['openstack']['human_readable_vm_names']).to be true
     end
 
     it 'template render succeeds if registry configured for bosh-init' do
-      manifest['properties']['registry']['endpoint'] = nil
-      manifest['properties']['registry']['host'] = '127.0.0.1'
-      manifest['properties']['registry']['port'] = 6901
-      manifest['properties']['openstack']['human_readable_vm_names'] = true
+      manifest['registry']['endpoint'] = nil
+      manifest['registry']['host'] = '127.0.0.1'
+      manifest['registry']['port'] = 6901
+      manifest['openstack']['human_readable_vm_names'] = true
 
       expect(subject['cloud']['properties']['registry']['endpoint']).to eq('http://127.0.0.1:6901')
       expect(subject['cloud']['properties']['openstack']['human_readable_vm_names']).to be true
@@ -222,144 +215,12 @@ describe 'cpi.json.erb' do
     [false, true].each do |prop|
       context "when anti-affinity is set to #{prop}" do
         it 'errors to inform the user this is no longer supported' do
-          manifest['properties']['openstack']['enable_auto_anti_affinity'] = prop
+          manifest['openstack']['enable_auto_anti_affinity'] = prop
 
           expect { subject }.to raise_error RuntimeError,
             "Property 'enable_auto_anti_affinity' is no longer supported. Please remove it from your configuration."
         end
       end
     end
-  end
-end
-
-class TemplateEvaluationContext
-  attr_reader :name, :index
-  attr_reader :properties, :raw_properties
-  attr_reader :spec
-  def initialize(spec, manifest)
-    @name = spec['job']['name'] if spec['job'].is_a?(Hash)
-    @index = spec['index']
-    properties = {}
-    spec['properties'].each do |name, x|
-      default = x['default']
-      copy_property(properties, manifest['properties'], name, default)
-    end
-    @properties = openstruct(properties)
-    @raw_properties = properties
-    @spec = openstruct(spec)
-  end
-
-  def recursive_merge(hash, other)
-    hash.merge(other) do |_, old_value, new_value|
-      if old_value.class == Hash && new_value.class == Hash
-        recursive_merge(old_value, new_value)
-      else
-        new_value
-      end
-    end
-  end
-
-  def get_binding
-    binding.taint
-  end
-
-  def p(*args)
-    names = Array(args[0])
-    names.each do |name|
-      result = lookup_property(@raw_properties, name)
-      return result unless result.nil?
-    end
-    return args[1] if args.length == 2
-    raise UnknownProperty, names
-  end
-
-  def if_p(*names)
-    values = names.map do |name|
-      value = lookup_property(@raw_properties, name)
-      return ActiveElseBlock.new(self) if value.nil?
-      value
-    end
-    yield *values
-    InactiveElseBlock.new
-  end
-
-  private
-
-  def copy_property(dst, src, name, default = nil)
-    keys = name.split('.')
-    src_ref = src
-    dst_ref = dst
-    keys.each do |key|
-      src_ref = src_ref[key]
-      break if src_ref.nil? # no property with this name is src
-    end
-    keys[0..-2].each do |key|
-      dst_ref[key] ||= {}
-      dst_ref = dst_ref[key]
-    end
-    dst_ref[keys[-1]] ||= {}
-    dst_ref[keys[-1]] = src_ref.nil? ? default : src_ref
-  end
-
-  def openstruct(object)
-    case object
-    when Hash
-      mapped = object.each_with_object({}) { |(k, v), h| h[k] = openstruct(v); }
-      OpenStruct.new(mapped)
-    when Array
-      object.map { |item| openstruct(item) }
-    else
-      object
-    end
-  end
-
-  def lookup_property(collection, name)
-    keys = name.split('.')
-    ref = collection
-    keys.each do |key|
-      ref = ref[key]
-      return nil if ref.nil?
-    end
-    ref
-  end
-
-  class UnknownProperty < StandardError
-    def initialize(names)
-      @names = names
-      super("Can't find property '#{names.join("', or '")}'")
-    end
-  end
-
-  class ActiveElseBlock
-    def initialize(template)
-      @context = template
-    end
-
-    def else
-      yield
-    end
-
-    def else_if_p(*names, &block)
-      @context.if_p(*names, &block)
-    end
-  end
-
-  class InactiveElseBlock
-    def else; end
-
-    def else_if_p(*_)
-      InactiveElseBlock.new
-    end
-  end
-end
-
-class ERBRenderer
-  def initialize(context)
-    @context = context
-  end
-
-  def render(erb_content)
-    erb = ERB.new(erb_content)
-    erb.result(@context.get_binding)
   end
 end
