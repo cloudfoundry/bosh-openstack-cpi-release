3b60856ba270952cb03362a32389492a201ed208
Danny Berger <dberger@pivotal.io>
2018-10-19 16:47:13 +0200

    Refactor some create_vm_spec tests
    Move them into vm_creator_spec to more closely match where the code
    lives.
    
    [#159790492](https://www.pivotaltracker.com/story/show/159790492)
    
    Co-authored-by: Vladimir Videlov <vladimir.videlov@sap.com>

--- src/bosh_openstack_cpi/spec/unit/create_vm_spec.rb
+++ src/bosh_openstack_cpi/spec/unit/create_vm_spec.rb
@@ -317,30 +317,6 @@ describe Bosh::OpenStackCloud::Cloud, 'create_vm' do
       end
     end
 
-    context 'when vm creation fails' do
-      before(:each) do
-        allow(cloud.compute.servers).to receive(:create).and_raise 'BOOM!!!'
-        allow_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:prepare)
-      end
-
-      it 'calls NetworkConfigurator#cleanup' do
-        expect_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:cleanup)
-
-        expect {
-          cloud.create_vm('agent-id', 'sc-id', resource_pool_spec, manual_network, nil, environment)
-        }.to raise_error RuntimeError, 'BOOM!!!'
-      end
-
-      context 'when NetworkConfigurator#cleanup fails' do
-        it 'fails with the vm creation failure' do
-          allow_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:cleanup).and_raise 'BOOM Cleanup!!!'
-
-          expect {
-            cloud.create_vm('agent-id', 'sc-id', resource_pool_spec, manual_network, nil, environment)
-          }.to raise_error RuntimeError, 'BOOM!!!'
-        end
-      end
-    end
     context 'when vm_destroy fails' do
       before(:each) do
         allow(server).to receive(:destroy).and_raise 'BOOM!!!'
@@ -915,84 +891,6 @@ describe Bosh::OpenStackCloud::Cloud, 'create_vm' do
           }.to raise_error(Bosh::Clouds::CloudError)
           expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-1'))
           expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-2'))
-        end
-      end
-    end
-
-    context 'manual network' do
-      let(:name) do
-        expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-1'))
-      end
-
-      let(:ports) { double('Fog::OpenStack::Network::Ports') }
-      let(:cloud) do
-        mock_cloud(options) do |openstack|
-          allow(openstack.compute.servers).to receive(:create).and_return(server)
-          allow(openstack.image.images).to receive(:find_by_id).and_return(image)
-          allow(openstack.compute.flavors).to receive(:find).and_return(flavor)
-          allow(openstack.compute.key_pairs).to receive(:find).and_return(key_pair)
-          allow(openstack.network).to receive(:ports).and_return(ports)
-        end
-      end
-      let(:network_spec) { manual_network_spec(ip: '10.0.0.1') }
-
-      before do
-        allow_any_instance_of(Bosh::OpenStackCloud::AvailabilityZoneProvider).to receive(:select_azs).and_return(azs)
-        allow_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:cleanup)
-
-        port_result_net = double(
-          'ports1',
-          id: '117717c1-81cb-4ac4-96ab-99aaf1be9ca8',
-          network_id: 'net',
-          mac_address: 'AA:AA:AA:AA:AA:AA',
-        )
-        expect(ports).to receive(:create).once.with(
-          network_id: 'net',
-          fixed_ips: [{ ip_address: '10.0.0.1' }],
-          security_groups: ['default_sec_group_id'],
-        ).and_return(port_result_net)
-      end
-
-      it 'iterates over the azs till it can create a vm in one of the configured azs' do
-        allow(cloud.compute.servers)
-          .to receive(:create)
-          .with(include(availability_zone: 'az-1'))
-          .and_raise Bosh::Clouds::CloudError.new('Example exception from infrastructure')
-
-        expect_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:prepare).once.and_call_original
-
-        cloud.create_vm('agent-id', 'sc-id', resource_pool_spec, { 'network_a' => network_spec }, nil, environment)
-
-        name
-        expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-2'))
-      end
-
-      context 'all azs fail' do
-        let(:err) { Bosh::Clouds::CloudError.new('Example exception from infrastructure') }
-        before { allow(cloud.compute.servers).to receive(:create).and_raise err }
-
-        it 'raises an error if last try fails' do
-          expect_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:cleanup)
-          expect_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:prepare).once.and_call_original
-
-          expect {
-            cloud.create_vm('agent-id', 'sc-id', resource_pool_spec, { 'network_a' => network_spec }, nil, environment)
-          }.to raise_error(Bosh::Clouds::CloudError)
-
-          expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-1'))
-          expect(cloud.compute.servers).to have_received(:create).with(include(availability_zone: 'az-2'))
-        end
-
-        it 'raises an error on network clean and provide log warning' do
-          cleanup_err = StandardError.new('Exception by network cleanup')
-
-          expect_any_instance_of(Bosh::OpenStackCloud::NetworkConfigurator).to receive(:cleanup).and_raise cleanup_err
-          expect(Bosh::Clouds::Config.logger).to receive(:warn)
-            .with("Failed to cleanup network resources: #{cleanup_err.message}")
-
-          expect {
-            cloud.create_vm('agent-id', 'sc-id', resource_pool_spec, { 'network_a' => network_spec }, nil, environment)
-          }.to raise_error(Bosh::Clouds::CloudError)
         end
       end
     end
--- src/bosh_openstack_cpi/spec/unit/vm_creator_spec.rb
+++ src/bosh_openstack_cpi/spec/unit/vm_creator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Bosh::OpenStackCloud::VmCreator do
   subject(:vm_creator) do
     described_class.new(
-      nil,
+      cloud,
       network_configurator,
       server,
       az_provider,
@@ -13,28 +13,58 @@ describe Bosh::OpenStackCloud::VmCreator do
     )
   end
 
+  let(:cloud) { double }
   let(:network_configurator) { instance_double(Bosh::OpenStackCloud::NetworkConfigurator, prepare: nil, cleanup: nil) }
   let(:server) { double }
-  let(:az_provider) { double }
+  let(:multiple_azs) { false }
+  let(:az_provider) do
+    instance_double(
+      Bosh::OpenStackCloud::AvailabilityZoneProvider,
+      use_multiple_azs?: multiple_azs,
+      select: nil,
+    )
+  end
   let(:cloud_properties) { {} }
   let(:agent_settings) { double }
   let(:create_vm_params) { {} }
+  let(:manual_network) { { 'network_a' => manual_network_spec(ip: '10.0.0.1') } }
+  let(:environment) { { 'test_env' => 'value' } }
 
   describe 'perform' do
     before do
-      allow(az_provider).to receive(:use_multiple_azs?).and_return(multiple_azs)
       allow(server).to receive(:create)
     end
 
-    context 'when using availability_zone' do
-      let(:multiple_azs) { false }
+    context 'when vm creation fails' do
+      before(:each) do
+        allow(server).to receive(:create).and_raise 'BOOM!!!'
+        allow(network_configurator).to receive(:prepare)
+      end
 
+      it 'cleans up network resources' do
+        expect(network_configurator).to receive(:cleanup)
+
+        expect {
+          subject.perform
+        }.to raise_error RuntimeError, 'BOOM!!!'
+      end
+
+      it 'ignores but warns about network resource cleanup failures' do
+        allow(network_configurator).to receive(:cleanup).and_raise 'BOOM Cleanup!!!'
+
+        expect {
+          subject.perform
+        }.to raise_error RuntimeError, 'BOOM!!!'
+      end
+    end
+
+    context 'when using availability_zone' do
       before do
         allow(az_provider).to receive(:select).and_return('az1')
       end
 
       it 'calls create server once' do
-        vm_creator.perform
+        subject.perform
         expect(server).to have_received(:create).with(anything, anything, anything, include(availability_zone: 'az1'))
       end
     end
@@ -53,20 +83,46 @@ describe Bosh::OpenStackCloud::VmCreator do
 
       context 'when the first create server fails' do
         it 'calls create server with the second zone and logs retrying' do
-          expect(server).to receive(:create).with(anything, anything, anything, include(availability_zone: 'az1')).and_raise('create-error')
+          expect(network_configurator).to receive(:prepare).once
+          expect(server).to receive(:create)
+            .with(anything, anything, anything, include(availability_zone: 'az1'))
+            .and_raise('create-error')
+
           expect(server).to receive(:create).with(anything, anything, anything, include(availability_zone: 'az2'))
+
           vm_creator.perform
-          expect(Bosh::Clouds::Config.logger).to have_received(:warn).with("Failed to create VM in AZ 'az1' with error 'create-error', retrying in a different AZ")
+
+          expect(Bosh::Clouds::Config.logger).to have_received(:warn)
+            .with("Failed to create VM in AZ 'az1' with error 'create-error', retrying in a different AZ")
         end
       end
 
       context 'when all create servers fails' do
+        before do
+          expect(network_configurator).to receive(:cleanup).once
+          expect(network_configurator).to receive(:prepare).once
+
+          expect(server).to receive(:create)
+            .with(anything, anything, anything, include(availability_zone: 'az1'))
+            .and_raise('create-error-1')
+          expect(server).to receive(:create)
+            .with(anything, anything, anything, include(availability_zone: 'az2'))
+            .and_raise('create-error-2')
+        end
+
         it 'raises an error and logs the number of retries' do
-          expect(server).to receive(:create).and_raise('create-error').twice
-          expect {
-            vm_creator.perform
-          }.to raise_error('create-error')
-          expect(Bosh::Clouds::Config.logger).to have_received(:error).with("Failed to create VM in AZ 'az2' with error 'create-error' after 2 retries. No AZs left to retry.")
+          expect { vm_creator.perform }.to raise_error('create-error-2')
+
+          expect(Bosh::Clouds::Config.logger).to have_received(:error)
+            .with("Failed to create VM in AZ 'az2' with error 'create-error-2' after 2 retries. No AZs left to retry.")
+        end
+
+        it 'ignores but warns about network resource cleanup failures' do
+          allow(network_configurator).to receive(:cleanup).and_raise 'BOOM Cleanup!!!'
+          expect(Bosh::Clouds::Config.logger).to receive(:warn)
+            .with('Failed to cleanup network resources: BOOM Cleanup!!!')
+
+          expect { subject.perform }.to raise_error('create-error-2')
         end
       end
     end
