a8fa225aa75db1df3c40d62c843b00b68ba385e4
Joseph Palermo <jpalermo@pivotal.io>
2021-09-04 07:07:33 -0700

    Move ci pipeline to new concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,11 +2,6 @@
 
 set -eu
 
-if [[ $(lpass status -q; echo $?) != 0 ]]; then
-  echo "Login with lpass first"
-  exit 1
-fi
-
-fly -t cpi set-pipeline -p "bosh-openstack-cpi" \
+fly -t bosh-ecosystem set-pipeline -p "bosh-openstack-cpi" \
     -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G --sync=now "bosh openstack cpi main pipeline secrets" --notes)
