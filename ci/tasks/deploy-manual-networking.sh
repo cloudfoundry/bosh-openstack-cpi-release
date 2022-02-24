de38aaf41d0e198242957f331dab61c881bb9f1e
Brian Cunnie <bcunnie@vmware.com>
2022-02-23 20:05:07 -0800

    🐞 Use `mkpasswd`, not Ruby, to create password hash
    Using Ruby's `String#crypt` to generate a SHA-512 password doesn't work
    on Focal Fossil (it generates "*0" instead of a real password hash), and
    the Ruby developers have deprecated that method for many good reasons.
    
    We now use `mkpasswd` as an alternative.
    
    Note that we use exactly 10 bytes as a salt to the SHA-512 hashing
    algorithm. This avoids `base32` padding the end of the salt with an "=",
    which is illegal. `base32` converts 10 bytes to exactly 16 characters,
    no padding.
    
    Fixes:
    ```
    invalid argument for flag `-v, --var' (expected []template.VarKV): Deserializing variables 'bosh_vcap_password_hash=*0': yaml: unknown anchor '0' referenced
    ```

--- ci/tasks/deploy-manual-networking.sh
+++ ci/tasks/deploy-manual-networking.sh
@@ -39,7 +39,7 @@ export_terraform_variable "director_public_ip"
 semver=`cat version-semver/number`
 cpi_release_name="bosh-openstack-cpi"
 deployment_dir="${PWD}/bosh-director-deployment"
-bosh_vcap_password_hash=$(ruby -rsecurerandom -e 'puts ENV["bosh_vcap_password"].crypt("$6$#{SecureRandom.base64(14)}")')
+bosh_vcap_password_hash=$(mkpasswd -m sha-512 -S $(dd if=/dev/random count=10 bs=1 | base32) "${bosh_vcap_password}")
 
 maybe_use_custom_ca_ops_file=""
 maybe_load_custom_ca_file=""
