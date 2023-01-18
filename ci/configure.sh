faa3f178d1d0e180448792c558e9a63afc02dbe2
Joseph Palermo <jpalermo@pivotal.io>
2023-01-17 22:19:31 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,4 +4,5 @@ set -eu
 
 fly -t bosh-ecosystem set-pipeline -p "bosh-openstack-cpi" \
     -c ci/pipeline.yml \
+    -v bosh_vcap_password=test_password_123 \
     --load-vars-from <(lpass show -G --sync=now "bosh openstack cpi main pipeline secrets" --notes)
