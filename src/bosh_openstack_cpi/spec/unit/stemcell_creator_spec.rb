4547ee12595b118aa1bfac56a5f766decff84893
Leo Scordilis <leoscordilis@gmail.com>
2024-09-06 11:55:30 -0400

    Add support for virtio-scsi via stemcell cloud properties
    References:
    
    - https://wiki.openstack.org/wiki/Documentation/HypervisorTuningGuide#Symptoms_of_Being_Disk_Bound
    - https://wiki.openstack.org/wiki/Documentation/HypervisorTuningGuide#Instance_and_Image_Configuration_4

--- src/bosh_openstack_cpi/spec/unit/stemcell_creator_spec.rb
+++ src/bosh_openstack_cpi/spec/unit/stemcell_creator_spec.rb
@@ -18,6 +18,20 @@ describe Bosh::OpenStackCloud::HeavyStemcellCreator do
       expect(subject.normalize_image_properties(properties)).to have_key(:version)
     end
 
+    it 'supports virtio-scsi properties' do
+      properties = {
+        'hw_disk_bus_model' => 'virtio-scsi',
+        'hw_scsi_model' => 'virtio-scsi',
+        'hw_disk_bus' => 'scsi',
+      }
+
+      expect(subject.normalize_image_properties(properties)).to include(
+        :hw_disk_bus_model,
+        :hw_scsi_model,
+        :hw_disk_bus,
+      )
+    end
+
     it 'maps hypervisor key to hypervisor_type' do
       properties = {
         'hypervisor' => 'kvm',
