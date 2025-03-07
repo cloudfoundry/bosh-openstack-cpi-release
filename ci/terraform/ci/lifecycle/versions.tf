59d37bff182b675aa069785cf20684a846c66d4c
Brian Upton <brian.upton@broadcom.com>
2025-03-06 16:17:50 -0800

    Constrain the provider version, not terraform
    Signed-off-by: Kenneth Lakin <kenneth.lakin@broadcom.com>

--- ci/terraform/ci/lifecycle/versions.tf
+++ ci/terraform/ci/lifecycle/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
+      version = ">= 2.1.0, < 3.0.0"
     }
   }
-  required_version = ">= 2.1.0, < 3.0.0"
+  required_version = ">= 0.13"
 }
