6df5ad8759147e2818959ec16fd0090326592363
Ned Petrov <nedd.petrov@gmail.com>
2026-08-25 10:31:07 +0300

    ci: fix shellcheck issues and improve utils.sh
    - Use bash indirect expansion (${!name}) instead of eval in optional_value
    - Quote all unquoted variable expansions (SC2086)
    - Separate local declarations from assignments (SC2155)
    - Replace backticks with $(...) (SC2006)
    - Use [ -n ] instead of [ ! -z ]
    - Replace cat | jq with jq file (UUOC)
    - Use export "name=val" pattern in export_terraform_variable
    - Remove dead wget+sha1 branch (old_bosh_with_registry_sha1 was never defined)

--- ci/tasks/utils.sh
+++ ci/tasks/utils.sh
@@ -10,19 +10,20 @@ creds_path() {
 
 optional_value() {
   local name=$1
-  local value=$(eval echo '$'$name)
+  local value
+  value="${!name}"
   if [ "$value" == 'replace-me' ] || [ "$value" == 'null' ]; then
     echo "unsetting optional environment variable $name"
-    unset $name
+    unset "$name"
   fi
 }
 
 init_openstack_cli_env(){
-    : ${BOSH_OPENSTACK_AUTH_URL:?}
-    : ${BOSH_OPENSTACK_USERNAME:?}
-    : ${BOSH_OPENSTACK_API_KEY:?}
-    : ${BOSH_OPENSTACK_PROJECT:?}
-    : ${BOSH_OPENSTACK_DOMAIN_NAME:?}
+    : "${BOSH_OPENSTACK_AUTH_URL:?}"
+    : "${BOSH_OPENSTACK_USERNAME:?}"
+    : "${BOSH_OPENSTACK_API_KEY:?}"
+    : "${BOSH_OPENSTACK_PROJECT:?}"
+    : "${BOSH_OPENSTACK_DOMAIN_NAME:?}"
     optional_value BOSH_OPENSTACK_CA_CERT
 
     export OS_DEFAULT_DOMAIN=$BOSH_OPENSTACK_DOMAIN_NAME
@@ -35,10 +36,10 @@ init_openstack_cli_env(){
     export OS_INTERFACE=$BOSH_OPENSTACK_INTERFACE
 
     if [ -n "$BOSH_OPENSTACK_CA_CERT" ]; then
-      tmpdir=$(mktemp -dt "$(basename $0).XXXXXXXXXX")
+      tmpdir=$(mktemp -dt "$(basename "$0").XXXXXXXXXX")
       cacert="$tmpdir/cacert.pem"
       echo "Writing cacert.pem to $cacert"
-      echo "$BOSH_OPENSTACK_CA_CERT" > $cacert
+      echo "$BOSH_OPENSTACK_CA_CERT" > "$cacert"
       export OS_CACERT=$cacert
     fi
 
@@ -51,38 +52,40 @@ prepare_bosh_release() {
 
     use_compiled_release=true
 
-    local s3_path_to_bosh_release=$(find_bosh_compiled_release ${distribution} ${bosh_release_version} ${stemcell_version})
+    local s3_path_to_bosh_release
+    s3_path_to_bosh_release=$(find_bosh_compiled_release "${distribution}" "${bosh_release_version}" "${stemcell_version}")
 
-    if [ ! -z ${s3_path_to_bosh_release} ];then
+    if [ -n "${s3_path_to_bosh_release}" ]; then
         echo "Using compiled BOSH release: s3://bosh-compiled-release-tarballs/$s3_path_to_bosh_release"
-        aws --no-sign-request s3 cp s3://bosh-compiled-release-tarballs/${s3_path_to_bosh_release} ${deployment_dir}/bosh-release.tgz
+        # shellcheck disable=SC2154
+        aws --no-sign-request s3 cp "s3://bosh-compiled-release-tarballs/${s3_path_to_bosh_release}" "${deployment_dir}/bosh-release.tgz"
     else
         use_compiled_release=false
     fi
 
-    if [ "${use_compiled_release}" = "false" ];then
+    if [ "${use_compiled_release}" = "false" ]; then
        echo "Using BOSH release from sources"
-       if [ -z ${bosh_release_version} ];then
-        cp ./bosh-release/*.tgz ${deployment_dir}/bosh-release.tgz
-       else
-         wget https://bosh.io/d/github.com/cloudfoundry/bosh?v=${bosh_release_version} -O ${deployment_dir}/bosh-release.tgz
-         echo "$old_bosh_with_registry_sha1 $deployment_dir/bosh-release.tgz" | sha1sum -c -
-       fi
+       # shellcheck disable=SC2154
+       cp ./bosh-release/*.tgz "${deployment_dir}/bosh-release.tgz"
     fi
 }
 
 find_bosh_compiled_release(){
     local distribution=$1
-    local bosh_release_version=${2:-`cat ./bosh-release/version`}
-    local stemcell_version=${3:-`cat ./stemcell-director/version`}
+    local bosh_release_version=${2:-$(cat ./bosh-release/version)}
+    local stemcell_version=${3:-$(cat ./stemcell-director/version)}
 
-    local s3_path_to_bosh_release=`aws --no-sign-request s3 ls s3://bosh-compiled-release-tarballs | grep -oE "[^ ](\w|-)*$bosh_release_version.+$distribution.+$stemcell_version.*\.tgz" | sort -r | head -1`
-    echo ${s3_path_to_bosh_release}
+    local s3_path_to_bosh_release
+    s3_path_to_bosh_release=$(aws --no-sign-request s3 ls s3://bosh-compiled-release-tarballs | grep -oE "[^ ](\w|-)*$bosh_release_version.+$distribution.+$stemcell_version.*\.tgz" | sort -r | head -1)
+    echo "${s3_path_to_bosh_release}"
 }
 
 export_terraform_variable() {
     local metadata=$1
     local variable_name=$2
     local prefix=$3
-    export ${prefix}${variable_name}=$(cat ${metadata} | jq -c --raw-output ".${variable_name}")
+    local varname="${prefix}${variable_name}"
+    local varval
+    varval=$(jq -c --raw-output ".${variable_name}" "${metadata}")
+    export "${varname}=${varval}"
 }
